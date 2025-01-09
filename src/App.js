eabf289b23097313fcf1c46ac988b9f75abd9d3d
ayodelefasulu <fasuluayodele@gmail.com>
2025-01-09 08:00:54 +0100

    Revert "added many other components: namely carousel, about section, portfolio, etc"
    This reverts commit b50c9f78bba51725f0b1cf9264f175d5aada6d02.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,25 @@
-//import logo from './logo.svg';
-//import { Navbar, NavbarBrand } from 'reactstrap';
+import logo from './logo.svg';
 import './App.css';
-import Carousel from './component/Carousel';
-import AboutSection from './component/About';
-import ServiceCard from './component/Servicecard';
-import PortfolioItem from './component/Portfolioitem';
 
-/*function App() {
+function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edited <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-*/
-
-
-const App = () => {
-  const services = [
-    { image: '/images/services-img-01.jpg', title: 'Analytics', description: 'Sapiente esse necessitatibus neque.' },
-    { image: '/images/services-img-02.jpg', title: 'Applications', description: 'Sapiente esse necessitatibus neque.' },
-    // Add other services here
-  ];
-
-  const portfolioItems = [
-    { image: '/images/portfolio-img-01.jpg', title: 'Financial Sustainability' },
-    { image: '/images/portfolio-img-02.jpg', title: 'Business Growth' },
-    // Add other portfolio items here
-  ];
-
-  return (
-    <div>
-      <Carousel />
-      <AboutSection />
-      <div className="services-bar container">
-        <h1 className="py-4">Our Best Services</h1>
-        <div className="row">
-          {services.map((service, index) => (
-            <ServiceCard key={index} {...service} />
-          ))}
-        </div>
-      </div>
-      <div className="portfolio-main container">
-        <h2>Our Portfolio</h2>
-        <div className="row">
-          {portfolioItems.map((item, index) => (
-            <PortfolioItem key={index} {...item} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default App;
