e0545518161bbb5c0f34519d79adcccc6da3aa38
ayodelefasulu <fasuluayodele@gmail.com>
2025-01-18 06:11:43 +0100

    movies exercise

---
+++ src/component/Movies.jsx
@@ -0,0 +1,7 @@
+import { getMovies } from "./services/fakeMoviesService";
+import { getGenre } from "../services/fakeGenreServices";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Movies extends Component {
+  
+}
